c24c5efddecc05d15c78beae282b92f8e474fdca
apigituser <responsemail850@gmail.com>
2025-06-12 17:34:47 +0530

    login and logout

--- TodoApp/urls.py
+++ TodoApp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register),
+    path('login/', views.loginUser),
+    path('logout/', views.logoutUser),
 ]
--- TodoApp/views.py
+++ TodoApp/views.py
@@ -1,10 +1,32 @@
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.http import JsonResponse, HttpRequest
 from django.views.decorators.csrf import csrf_exempt
 import json
 
 @csrf_exempt
-def register(request: HttpRequest):
+def loginUser(request):
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    
+    if username and password:
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return JsonResponse({'success': f'{username} logged in'})
+        return JsonResponse({'401': 'invalid credentials'})
+    return JsonResponse({'you idiot': 'provide a username and password'})
+
+@csrf_exempt
+def logoutUser(request):
+    if request.user.is_authenticated:
+        authenticated_user = str(request.user)
+        logout(request)
+        return JsonResponse({'200': f'{authenticated_user} logged out'})
+    return JsonResponse({'bish': 'you\'re anonymous'})
+
+@csrf_exempt
+def register(request):
     body = request.body
     user = None
     try:
